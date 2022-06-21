fd7071cca516b58900c54893e812bde0436119d4
Hayden OH <hayden@gna.company>
2022-06-21 17:41:11 +0900

    GET X internalizion

--- gex_multi_lang_example/lib/next_page.dart
+++ gex_multi_lang_example/lib/next_page.dart
@@ -6,8 +6,9 @@ class NextPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SafeArea(
+        child: Container(
       child: Scaffold(body: Text('enter_mail'.tr)),
-    );
+    ));
   }
 }
